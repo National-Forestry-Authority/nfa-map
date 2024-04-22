c3f3b62020c790dd54a32133dde3c6333d607bb1
miriam <miriam@cambrico.net>
2024-04-22 12:32:07 +0200

    Allow for custom colors

--- src/styles/index.js
+++ src/styles/index.js
@@ -20,7 +20,22 @@ const colors = {
 
 // Returns an OpenLayers Style for a given color.
 const colorStyles = (color) => {
-  const rgba = colors[color] ? colors[color] : colors.yellow;
+  let rgba = colors[color] || colors.yellow;
+
+  // Allow for custom colors.
+  if (color.startsWith('#')) {
+    if (color.length === 7) {
+      rgba = `rgba(${parseInt(color.slice(1, 3), 16)},${parseInt(color.slice(3, 5), 16)},${parseInt(color.slice(5, 7), 16)} ,1`;
+    } else if (color.length === 9) {
+      rgba = `rgba(${parseInt(color.slice(1, 3), 16)},${parseInt(color.slice(3, 5), 16)},${parseInt(color.slice(5, 7), 16)},${parseInt(color.slice(7, 9), 16)}`;
+    }
+  } else if (color.startsWith('rgba')) {
+    rgba = color;
+  }
+  else if (color.startsWith('rgb')) {
+    rgba = color.replace('rgb', 'rgba').replace(')', ',1)');
+  }
+
   const stroke = new Stroke({
     color: rgba,
     width: 2,
