5ec077a11d00cb0cb00177d1319ededf955f532e
miriam <miriam@cambrico.net>
2024-04-24 17:01:05 +0200

    Improve custom color logic

--- src/styles/index.js
+++ src/styles/index.js
@@ -20,24 +20,10 @@ const colors = {
 
 // Returns an OpenLayers Style for a given color.
 const colorStyles = (color) => {
-  let rgba = colors[color] || colors.yellow;
-
-  // Allow for custom colors.
-  if (color.startsWith('#')) {
-    if (color.length === 7) {
-      rgba = `rgba(${parseInt(color.slice(1, 3), 16)},${parseInt(color.slice(3, 5), 16)},${parseInt(color.slice(5, 7), 16)} ,1`;
-    } else if (color.length === 9) {
-      rgba = `rgba(${parseInt(color.slice(1, 3), 16)},${parseInt(color.slice(3, 5), 16)},${parseInt(color.slice(5, 7), 16)},${parseInt(color.slice(7, 9), 16)}`;
-    }
-  } else if (color.startsWith('rgba')) {
-    rgba = color;
-  }
-  else if (color.startsWith('rgb')) {
-    rgba = color.replace('rgb', 'rgba').replace(')', ',1)');
-  }
+  color = color.startsWith('#') || color.startsWith('rgb') ? color : colors[color] || colors.yellow;
 
   const stroke = new Stroke({
-    color: rgba,
+    color: color,
     width: 2,
   });
   const fill = new Fill({
