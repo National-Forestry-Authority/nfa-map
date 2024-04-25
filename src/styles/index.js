1e67f3c27fd105d2b9ee31caef57aa236d6d8e0a
miriam <miriam@cambrico.net>
2024-04-25 09:22:55 +0200

    Better custom color logic and updated readme

--- src/styles/index.js
+++ src/styles/index.js
@@ -20,10 +20,17 @@ const colors = {
 
 // Returns an OpenLayers Style for a given color.
 const colorStyles = (color) => {
-  color = color.startsWith('#') || color.startsWith('rgb') ? color : colors[color] || colors.yellow;
+  let strokeColor;
+  if (!color) {
+    strokeColor = colors.yellow;
+  } else if (colors[color]) {
+    strokeColor = colors[color];
+  } else {
+    strokeColor = color;
+  }
 
   const stroke = new Stroke({
-    color: color,
+    color: strokeColor,
     width: 2,
   });
   const fill = new Fill({
